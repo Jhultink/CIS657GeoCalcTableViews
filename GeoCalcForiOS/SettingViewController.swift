4262e69ccd1410bc79e12806e5f847b031dd5a48
Jie Tao <neo_aksa@hotmail.com>
2018-05-17 00:45:37 -0400

    Revert to origal

--- GeoCalcForiOS/SettingViewController.swift
+++ GeoCalcForiOS/SettingViewController.swift
@@ -8,34 +8,117 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate {
+    func settingsChanged(distanceUnits: String, bearingUnits: String)
+}
+
 class SettingViewController: UIViewController {
-
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var bearingLabel: UILabel!
+    
+    var selectedLabel: UILabel! = nil;
+    
+    let distanceUnits = ["Kilometers", "Miles"]
+    let bearingUnits = ["Degrees", "Mils"]
+    
+    var selectedDistance:String?
+    var selectedBearing:String?
+    
+    var currentPickerList:[String] = [String]();
+    //var selection: String = ""
+    
+    var delegate : SettingsViewControllerDelegate?
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
     @IBAction func cancelPressed(_ sender: Any) {
-        //navigationController!.popViewController(animated: true)
         dismiss(animated: true, completion: nil)
-
+    }
+    
+    @IBAction func savedPressed(_ sender: Any) {
+        if let d = self.delegate {
+            d.settingsChanged(distanceUnits: self.selectedDistance!, bearingUnits: self.selectedBearing!)
+        }
+        dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.selectedDistance = self.distanceUnits[0]
+        self.selectedBearing = self.bearingUnits[0]
+        
+        // Distance label tapped
+        let distanceTapped = UITapGestureRecognizer(target: self, action: #selector(distanceLabelTapped))
+        distanceLabel.addGestureRecognizer(distanceTapped)
+        
+        // Bearing label tapped
+        let bearingTapped = UITapGestureRecognizer(target: self, action: #selector(bearingLabelTapped))
+        bearingLabel.addGestureRecognizer(bearingTapped)
+        
+        let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.hidePicker))
+        self.view.addGestureRecognizer(detectTouch)
+        
+        self.currentPickerList = self.bearingUnits;
+        self.picker.isHidden = true;
+        self.picker.dataSource = self;
+        self.picker.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func distanceLabelTapped() {
+        self.selectedLabel = self.distanceLabel;
+        self.currentPickerList = self.distanceUnits;
+        picker.isHidden = false;
+        picker.dataSource = self;
+        
+        picker.selectRow(currentPickerList.index(of: self.selectedDistance!)!, inComponent: 0, animated: true)
     }
-    */
+    
+    @objc func bearingLabelTapped() {
+        self.selectedLabel = self.bearingLabel;
+        self.currentPickerList = self.bearingUnits;
+        picker.isHidden = false;
+        picker.dataSource = self
+        
+        picker.selectRow(currentPickerList.index(of: self.selectedBearing!)!, inComponent: 0, animated: true)
+    }
+    
+    @objc func hidePicker() {
+        picker.isHidden = true;
+    }
+}
 
+extension SettingViewController : UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return currentPickerList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return self.currentPickerList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        self.selectedLabel.text = self.currentPickerList[row]
+        
+        if self.selectedLabel == self.bearingLabel {
+            self.selectedBearing = self.currentPickerList[row]
+        } else {
+            self.selectedDistance = self.currentPickerList[row]
+        }
+    }
 }
--- GeoCalcForiOS/ViewController.swift
+++ GeoCalcForiOS/ViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 import CoreLocation
 
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SettingsViewControllerDelegate {
     
     @IBOutlet weak var latitudeP1: DecimalMinusTextField!
     @IBOutlet weak var latitudeP2: DecimalMinusTextField!
@@ -20,8 +19,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var bearingLabel: UILabel!
     
+    var bearingUnit = "Degrees"
+    var distanceUnit = "Kilometers"
+    
+    var distanceInKilometers:Double?
+    var bearingInDegrees:Double?
+    
     @IBAction func calculatePressed(_ sender: Any) {
+        calculateDistanceAndBearing()
+    }
+    
+    @IBAction func clearPressed(_ sender: Any) {
+        latitudeP1.text = ""
+        latitudeP2.text = ""
+        longitudeP1.text = ""
+        longitudeP2.text = ""
         
+        distanceLabel.text = "Distance: "
+        bearingLabel.text = "Bearing: "
+        
+        distanceInKilometers = nil
+        bearingInDegrees = nil
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(detectTouch)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func calculateDistanceAndBearing() {
         guard let latP1 = latitudeP1.text, let latP2 = latitudeP2.text, let lonP1 = longitudeP1.text, let lonP2 = longitudeP2.text else {
             
             let alert = UIAlertController(title: "Invalid value", message: "Please enter values for all fields", preferredStyle: .alert)
@@ -50,39 +83,42 @@ class ViewController: UIViewController {
         let distanceInMeters = p1.distance(from: p2)
         print(distanceInMeters)
         
-        distanceLabel.text = "Distance: " + String(format: "%.2f", distanceInMeters / 1000)
-        
-        bearingLabel.text = "Bearing: " + String(format: "%.2f", RadiansToDegrees(radians: Double(getBearing(fromLoc: p2, toLoc: p1))))
-        
-        
+        self.distanceInKilometers = (distanceInMeters / 1000)
+        self.bearingInDegrees = RadiansToDegrees(radians: Double(getBearing(fromLoc: p2, toLoc: p1)))
+    
+        redrawLabels()
     }
     
-    @IBAction func clearPressed(_ sender: Any) {
+    func redrawLabels() {
+        guard let dist = self.distanceInKilometers, let bear = self.bearingInDegrees else {
+            return
+        }
         
-        latitudeP1.text = ""
-        latitudeP2.text = ""
-        longitudeP1.text = ""
-        longitudeP2.text = ""
+        var recalculatedDist = dist
+        var recalculatedBearing = bear
         
+        if (self.bearingUnit == "Mils") {
+            recalculatedBearing *= 17.7777777778
+        }
+        
+        if (self.distanceUnit == "Miles") {
+            recalculatedDist *= 0.621371
+        }
+        
+        distanceLabel.text = "Distance: " + String(format: "%.2f", recalculatedDist) + " " + distanceUnit
+        bearingLabel.text = "Bearing: " + String(format: "%.2f", recalculatedBearing) + " " + bearingUnit
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
     }
     
     func d2r(degrees : Double) -> Double {
-        return degrees * M_PI / 180.0
+        return degrees * Double.pi / 180.0
     }
     
     func RadiansToDegrees(radians : Double) -> Double {
-        return radians * 180.0 / M_PI
+        return radians * 180.0 / Double.pi
     }
     
     func getBearing(fromLoc : CLLocation, toLoc : CLLocation) -> CGFloat {
@@ -92,10 +128,23 @@ class ViewController: UIViewController {
         let tLat = d2r(degrees: toLoc.coordinate.latitude)
         let tLng = d2r(degrees: toLoc.coordinate.longitude)
         
-        var a = CGFloat(sin(fLng-tLng)*cos(tLat));
-        var b = CGFloat(cos(fLat)*sin(tLat)-sin(fLat)*cos(tLat)*cos(fLng-tLng))
+        let a = CGFloat(sin(fLng-tLng)*cos(tLat));
+        let b = CGFloat(cos(fLat)*sin(tLat)-sin(fLat)*cos(tLat)*cos(fLng-tLng))
         
         return atan2(a,b)
+    }
+    
+    func settingsChanged(distanceUnits: String, bearingUnits: String) {
+        self.bearingUnit = bearingUnits
+        self.distanceUnit = distanceUnits
+        redrawLabels()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let nav = segue.destination as? UINavigationController, let dest = nav.topViewController as? SettingViewController {
+            dest.delegate = self
+        }
     }
 }
 
